d621c195405c1a356fb36de36b216b1ab0ff52a6
Ivan Bussio <ivanbussio96@gmail.com>
2026-04-28 22:38:23 +0200

    fix: convert workouts to ts

--- client/src/utils/workouts.ts
+++ client/src/utils/workouts.ts
@@ -1,9 +1,9 @@
-export const getWorkouts = () => {
+export const getWorkouts = (): any[] => {
   const data = localStorage.getItem("workouts");
   return data ? JSON.parse(data) : [];
 };
 
-export const addWorkout = (workout) => {
+export const addWorkout = (workout: any): any[] => {
   const workouts = getWorkouts();
   const newWorkout = { id: Date.now(), ...workout };
   const updated = [...workouts, newWorkout];
@@ -11,7 +11,7 @@ export const addWorkout = (workout) => {
   return updated;
 };
 
-export const deleteWorkout = (id) => {
+export const deleteWorkout = (id: number): any[] => {
   const workouts = getWorkouts();
   const updated = workouts.filter((w) => w.id !== id);
   localStorage.setItem("workouts", JSON.stringify(updated));
